7bac71e3db7407ed328fbf86d266ef44bbf5c25b
Ziteng Jiao <zjiao19@outlook.com>
2021-09-30 00:10:26 -0400

    update dataset builder

--- nanodet/data/dataset/__init__.py
+++ nanodet/data/dataset/__init__.py
@@ -1,9 +1,27 @@
 import copy
+import warnings
+
 from .coco import CocoDataset
+from .xml_dataset import XMLDataset
 
 
 def build_dataset(cfg, mode):
     dataset_cfg = copy.deepcopy(cfg)
-    if dataset_cfg['name'] == 'coco':
-        dataset_cfg.pop('name')
+    name = dataset_cfg.pop("name")
+    if name == "coco":
+        warnings.warn(
+            "Dataset name coco has been deprecated. Please use CocoDataset instead."
+        )
         return CocoDataset(mode=mode, **dataset_cfg)
+    elif name == "xml_dataset":
+        warnings.warn(
+            "Dataset name xml_dataset has been deprecated. "
+            "Please use XMLDataset instead."
+        )
+        return XMLDataset(mode=mode, **dataset_cfg)
+    elif name == "CocoDataset":
+        return CocoDataset(mode=mode, **dataset_cfg)
+    elif name == "XMLDataset":
+        return XMLDataset(mode=mode, **dataset_cfg)
+    else:
+        raise NotImplementedError("Unknown dataset type!")
